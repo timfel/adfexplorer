a1857134aabe1390fe33f38f4694aa31a7f057da
Tim Felgentreff <tim.felgentreff@oracle.com>
2024-03-27 09:43:49 +0100

    split launchers for os specific SWT dependency

--- ADFLister.java
+++ ADFLister.java
@@ -1,35 +1,8 @@
-///usr/bin/env jbang "$0" "$@" ; exit $?
-
-/* 
- * The META-INF directory can be re-generated running jbang in Java mode with and commenting out the FILES directive
- *   JAVA_TOOL_OPTIONS=-agentlib:native-image-agent=config-output-dir=META-INF/native-image/guitrace{pid}/
- */
-//FILES META-INF/native-image=META-INF/native-image
-
-/* 
- * Adapt the SWT dependency depending on the platform
+/*
+ * Dependencies are here again just so JBang edit works nicely in VSCode.
  */
 //DEPS org.eclipse.platform:org.eclipse.swt.gtk.linux.x86_64:3.125.0
-////DEPS org.eclipse.platform:org.eclipse.swt.win32.win32.x86_64:3.125.0
-////DEPS org.eclipse.platform:org.eclipse.swt.cocoa.macosx.x86_64:3.125.0
-////DEPS org.eclipse.platform:org.eclipse.swt.cocoa.macosx.aarch64:3.125.0
-
-/*
- * To reduce the size of the resulting binary, these options can be used when creating
- * a native image (see https://www.graalvm.org/latest/reference-manual/python/native-applications/)
- *   -Dpython.WithoutSSL=true
- *   -Dpython.WithoutDigest=true
- *   -Dpython.WithoutPlatformAccess=true
- *   -Dpython.WithoutCompressionLibraries=true
- *   -Dpython.WithoutNativePosix=true
- *   -Dpython.WithoutJavaInet=true
- *   -Dimage-build-time.PreinitializeContexts=
- */
-//DEPS org.graalvm.python:python-language:24.0.0
-//DEPS org.graalvm.python:python-resources:24.0.0
-//DEPS org.graalvm.python:python-launcher:24.0.0
 //DEPS org.graalvm.python:python-embedding:24.0.0
-//PIP amitools
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
--- ADFListerLinux.java
+++ ADFListerLinux.java
@@ -1,28 +1,16 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 
-/* 
- * The META-INF directory can be re-generated running jbang in Java mode with and commenting out the FILES directive
- *   JAVA_TOOL_OPTIONS=-agentlib:native-image-agent=config-output-dir=META-INF/native-image/linux{pid}/
- */
-//FILES META-INF/native-image/linux=META-INF/native-image/linux
 //DEPS org.eclipse.platform:org.eclipse.swt.gtk.linux.x86_64:3.125.0
-//SOURCES ADFLister.java
-
-/*
- * To reduce the size of the resulting binary, these options can be used when creating
- * a native image (see https://www.graalvm.org/latest/reference-manual/python/native-applications/)
- *   -Dpython.WithoutSSL=true
- *   -Dpython.WithoutDigest=true
- *   -Dpython.WithoutPlatformAccess=true
- *   -Dpython.WithoutCompressionLibraries=true
- *   -Dpython.WithoutNativePosix=true
- *   -Dpython.WithoutJavaInet=true
- *   -Dimage-build-time.PreinitializeContexts=
- */
 //DEPS org.graalvm.python:python-language:24.0.0
 //DEPS org.graalvm.python:python-resources:24.0.0
 //DEPS org.graalvm.python:python-launcher:24.0.0
 //DEPS org.graalvm.python:python-embedding:24.0.0
+/* 
+ * The META-INF directory can be re-generated running jbang in Java mode with and commenting out the FILES directive
+ *   JAVA_TOOL_OPTIONS=-agentlib:native-image-agent=config-output-dir=META-INF/native-image/swt/{pid}/
+ */
+//FILES META-INF/native-image/swt/=META-INF/native-image/swt
+//SOURCES ADFLister.java
 //PIP amitools
 
 public class ADFListerLinux {
